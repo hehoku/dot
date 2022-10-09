15fd3cbcacaa34e5252fd0ff5e81c579e7e9e093
hehoku <hehoku@outlook.com>
2022-10-09 22:17:38 +0800

    init

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,34 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Client } from "@notionhq/client";
+
+require("dotenv").config();
+
+const notion = new Client({ auth: process.env.NOTION_KEY });
+const databaseId = process.env.NOTION_DATABASE_ID;
+
+async function addItem(text) {
+  try {
+    const response = await notion.pages.create({
+      parent: { database_id: databaseId },
+      properties: {
+        title: {
+          title: [
+            {
+              text: {
+                content: text,
+              },
+            },
+          ],
+        },
+      },
+    });
+    console.log(response);
+    console.log("Success! Entry added.");
+  } catch (error) {
+    console.error(error.body);
+  }
+}
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  addItem("test");
+  res.status(200).json({ name: "John Doe" });
 }
